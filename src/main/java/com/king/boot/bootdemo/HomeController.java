d9bb90d23c9758fb3bc2084f199b0c8f3e0178d6
王君 <KingKingJun@hotmail.com>
2019-01-21 14:28:42 +0800

    项目运行起来

---
+++ src/main/java/com/king/boot/bootdemo/HomeController.java
@@ -0,0 +1,14 @@
+package com.king.boot.bootdemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("hello")
+    public String hello(){
+        return "Hello JingYi";
+    }
+
+}
