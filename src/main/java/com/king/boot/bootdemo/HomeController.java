ac6d8f64ceb06f61249ff9e6c6d83205c7eb3279
王君 <KingKingJun@hotmail.com>
2019-01-21 15:16:32 +0800

    配置注入

--- src/main/java/com/king/boot/bootdemo/HomeController.java
+++ src/main/java/com/king/boot/bootdemo/HomeController.java
@@ -1,14 +1,24 @@
 package com.king.boot.bootdemo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @Value("${name}")
+    private String name;
+
+    @Value("${age}")
+    private Integer age;
+
+    @Value("${content}")
+    private String content;
+
     @RequestMapping("hello")
     public String hello(){
-        return "Hello JingYi";
+        return "hello"+content;
     }
 
 }
