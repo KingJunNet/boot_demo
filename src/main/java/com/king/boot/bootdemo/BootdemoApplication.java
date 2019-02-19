a5523bca3f571341a1f3a63f8cd6ea8dc1acc1e3
王君 <KingKingJun@hotmail.com>
2019-02-19 11:18:35 +0800

    作业：添加仓储后运行成功

--- src/main/java/com/king/boot/bootdemo/BootdemoApplication.java
+++ src/main/java/com/king/boot/bootdemo/BootdemoApplication.java
@@ -1,8 +1,10 @@
 package com.king.boot.bootdemo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.king.boot.bootdemo.mapper")
 @SpringBootApplication
 public class BootdemoApplication {
 
