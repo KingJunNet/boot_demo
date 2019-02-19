1d13a85638f38820cef9613007560a394c2bb685
王君 <KingKingJun@hotmail.com>
2019-02-19 11:43:54 +0800

    作业：添加仓储后全流程跑通

--- src/main/java/com/king/boot/bootdemo/service/AssignmentQueryService.java
+++ src/main/java/com/king/boot/bootdemo/service/AssignmentQueryService.java
@@ -1,12 +1,18 @@
 package com.king.boot.bootdemo.service;
 
 import com.king.boot.bootdemo.entity.Assignment;
+import com.king.boot.bootdemo.mapper.AssignmentMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AssignmentQueryService {
 
+    @Autowired
+    private AssignmentMapper assignmentRepository;
+
     public Assignment getById(int id){
-        return new Assignment(id,String.format("king%d",id),1);
+//        return new Assignment(id,String.format("king%d",id),1);
+        return this.assignmentRepository.getById(id);
     }
 }
